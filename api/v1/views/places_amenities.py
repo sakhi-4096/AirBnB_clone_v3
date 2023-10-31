6c5206409dd217fa17a2b3a04d569458f8ecb4fa
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-31 03:12:01 +0200

    Add Swagger API documentation of endpoints for place amenities.

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -5,10 +5,12 @@ from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import abort, jsonify, make_response
+from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
 
 
 @app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+@swag_from('docs/place_amenity/get_places_amenities.yml', methods=['GET'])
 def get_place_amenities(place_id):
     """
     Retrieves the list of all Amenity objects of a Place
@@ -28,6 +30,7 @@ def get_place_amenities(place_id):
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('docs/place_amenity/delete_place_amenity.yml', methods=['DELETE'])
 def delete_place_amenity(place_id, amenity_id):
     """
     Deletes a Amenity object of a Place
@@ -56,6 +59,7 @@ def delete_place_amenity(place_id, amenity_id):
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+@swag_from('docs/place_amenity/post_place_amenity.yml', methods=['POST'])
 def post_place_amenity(place_id, amenity_id):
     """
     Link a Amenity object to a Place
